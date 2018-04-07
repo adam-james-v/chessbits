751ea7d19f280f92b24c2c8d4dd35fe66dc14874
Adam Vermeer <adam.vermeer2@gmail.com>
2018-04-07 15:16:17 +0000

    continuing to work on app version

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import chessbits
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='./web_view')
+from flask import Flask, render_template, send_file
+app = Flask(__name__, template_folder='./web_view', static_url_path='/web_view')
 
 @app.route("/")
 def hello():
     a = chessbits.specs('pawn')
     return str(a)
+
+@app.route("/pawn")
+def DownloadPawn():
+    return send_file('output/pawn.stl')
 
 @app.route("/view/")
 def view():
